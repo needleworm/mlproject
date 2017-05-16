ec7de058e6dfda29aa6ade635b0e125b6cedf611
sjna <monokai@kaist.ac.kr>
2017-05-17 01:37:10 +0900

    implement Datareader and testing

---
+++ Datareader.py
@@ -0,0 +1,52 @@
+import scipy
+import os
+#import PIL
+from PIL import Image
+import numpy as np
+import scipy as sp
+class Dataset:
+    def __init__(self,path,input_shape=(1024,1024),gt_shape=(1024,1024)):
+        self.input_shape=input_shape
+        self.gt_shape=gt_shape
+        self.path=path
+        if not os.path.exists(path):
+            print("path is wrong!")
+        else:
+            for file in os.listdir(path):
+                if file.endswith('.jpeg'):
+                    self.files.append(file)
+                    self.max_idx+=1
+    def next_batch(self,batch_size):
+        in_image=[]
+        gt_image=[]
+        cur_idx=self.cur_idx
+        print(cur_idx)
+        for i in range(cur_idx,cur_idx+batch_size):
+            if not i <self.max_idx:
+                break
+            path = self.path+self.files[i]
+            image = np.asarray(Image.open(path))
+#            resized_image = sp.ndimage.zoom(image,
+            in_image.append(image)
+            gt_image.append(image)
+        in_image = np.array(in_image)
+        gt_image=np.array(gt_image)
+        self.cur_idx=cur_idx+batch_size
+        if not self.cur_idx < self.max_idx:
+            self.cur_idx=0
+        return in_image,gt_image        
+
+    input_shape=None
+    gt_shape=None
+    cur_idx=0
+    max_idx=0
+    files=[] 
+
+
+def main():
+    a=Dataset("images/",(512,512),(1024,1024))
+    b,c=a.next_batch(1)
+    b,c=a.next_batch(5)
+    print(b.shape)
+    
+main()
