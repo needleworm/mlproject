94749ccf5a3679fad2111fe13ec2b39d15294446
needleworm <bhban@kakao.com>
2017-05-22 16:15:20 +0900

    add random_batch

--- Datareader.py
+++ Datareader.py
@@ -5,6 +5,8 @@ from PIL import Image
 import numpy as np
 import scipy as sp
 import skimage 
+import random
+
 class Dataset:
     def __init__(self,path,input_shape=(1024,1024),gt_shape=(1024,1024)):
         self.input_shape=input_shape
@@ -27,34 +29,55 @@ class Dataset:
         return ret
     def change_format(self,image):
        return ((image*255)/np.max(image)).astype('uint8')
+    def get_batch_inputs(self,path,idx):
+        path = self.path+self.files[idx]
+        i_image = self.read_image(path,self.input_shape,64)
+        poissonNoise = np.random.poisson(50,i_image.shape).astype(float) # noise model
+        i_image=i_image+poissonNoise # add noise to image 
+        g_image = self.read_image(path,self.gt_shape,64)
+        return i_image,g_image
 
     def next_batch(self,batch_size):
         in_image=[]
         gt_image=[]
         cur_idx=self.cur_idx
-        for i in range(cur_idx,cur_idx+batch_size):
-            if not i <self.max_idx:
-                break
-            path = self.path+self.files[i]
-            input_image = self.read_image(path,self.input_shape,64)
-            poissonNoise = np.random.poisson(50,input_image.shape).astype(float) # noise model
-            input_image=input_image+poissonNoise # add noise to image 
-            g_image = self.read_image(path,self.gt_shape,64)
-            formatted = self.change_format(input_image)
-            ret = Image.fromarray(formatted)
-            ret.show()
-            formatted = self.change_format(g_image)
-            ret = Image.fromarray(formatted)
-            ret.show()
-            in_image.append(input_image)
+        for i in range(batch_size):
+            idx = cur_idx+i
+            if not idx <self.max_idx:
+                idx=0
+            path = self.path+self.files[idx]
+            i_image,g_image = self.get_batch_inputs(path,idx)
+   # for debug
+   #        formatted = self.change_format(i_image)
+   #        ret = Image.fromarray(formatted)
+   #        ret.show()
+   #        formatted = self.change_format(g_image)
+   #        ret = Image.fromarray(formatted)
+   #        ret.show()
+            in_image.append(i_image)
             gt_image.append(g_image)
-
         in_image = np.array(in_image)
         gt_image=np.array(gt_image)
-        self.cur_idx=cur_idx+batch_size
-        if not self.cur_idx < self.max_idx:
-            self.cur_idx=0
+        self.cur_idx=(cur_idx+batch_size)%self.max_idx # update for next batching
         return in_image,gt_image        
+    def random_batch(self,batch_size):
+        in_image=[]
+        gt_image=[]
+        cur_idx = random.random(0,max_idx)
+        for i in range(batch_size):
+            if not cur_idx < self.max_idx:
+                cur_idx=0
+            path =self.path+self.files[cur_idx]
+            i_image,g_image=self.get_batch_inputs(path,cur_idx)
+            in_image.append(i_image)
+            gt_image.append(g_image)
+            cur_idx+=1
+        in_image = np.array(in_image)
+        gt_image=np.array(gt_image)
+        self.cur_idx=(cur_idx+batch_size)%self.max_idx # update for next batching 
+        return in_image,gt_image        
+
+
     input_shape=None
     gt_shape=None
     cur_idx=0
