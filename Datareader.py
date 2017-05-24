794944a293618d222d9488d4bc9494affaf137dc
HayeonLee <yeonhi926@gmail.com>
2017-05-24 17:39:37 +0900

    merge

--- Datareader.py
+++ Datareader.py
@@ -7,43 +7,53 @@ import scipy as sp
 import skimage 
 import random
 
+
 class Dataset:
-    def __init__(self,path,input_shape=(1024,1024),gt_shape=(1024,1024)):
+    def __init__(self, path, input_shape=(1024,1024), gt_shape=(1024,1024)):
         self.input_shape=input_shape
         self.gt_shape=gt_shape
-        self.path=path
+        self.path = path
         if not os.path.exists(path):
             print("path is wrong!")
         else:
             for file in os.listdir(path):
                 if file.endswith('.jpeg'):
                     self.files.append(file)
-                    self.max_idx+=1
-    def read_image(self,path,size,option):   # this function reads image as float64
+                    self.max_idx += 1
+
+    def read_image(self, path, size, option):   # this function reads image as float64
         if option==64:
             image =(scipy.misc.imread(path)).astype(float)
             ret=scipy.misc.imresize(image,size).astype(float)
+        elif option==32:
+            image =(scipy.misc.imread(path)).astype('float32')
+            ret=scipy.misc.imresize(image,size).astype('float32')
         elif option==8:
             image = Image.open(path)
             ret = np.asarray( image.resize(size,Image.ANTIALIAS))
         return ret
+
     def change_format(self,image):
        return ((image*255)/np.max(image)).astype('uint8')
-    def get_batch_inputs(self,path,idx):
+
+    def get_batch_inputs(self, path, idx, option):
         path = self.path+self.files[idx]
-        i_image = self.read_image(path,self.input_shape,64)
-        poissonNoise = np.random.poisson(50,i_image.shape).astype(float) # noise model
+        i_image = self.read_image(path,self.input_shape, option)
+        if option==64:
+            poissonNoise = np.random.poisson(50, i_image.shape).astype(float) # noise model
+        elif option==32:
+            poissonNoise = np.random.poisson(50, i_image.shape).astype('float32') # noise model
         i_image=i_image+poissonNoise # add noise to image 
-        g_image = self.read_image(path,self.gt_shape,64)
+        g_image = self.read_image(path,self.gt_shape, option)
         return i_image,g_image
 
-    def next_batch(self,batch_size):
+    def next_batch(self, batch_size, option):
         in_image=[]
         gt_image=[]
         cur_idx=self.cur_idx
         for i in range(batch_size):
             path = self.path+self.files[cur_idx]
-            i_image,g_image = self.get_batch_inputs(path,cur_idx)
+            i_image,g_image = self.get_batch_inputs(path, cur_idx, option)
    # for debug
    #        formatted = self.change_format(i_image)
    #        ret = Image.fromarray(formatted)
@@ -57,14 +67,15 @@ class Dataset:
         in_image = np.array(in_image)
         gt_image=np.array(gt_image)
         self.cur_idx=cur_idx # update for next batching
-        return in_image,gt_image        
-    def random_batch(self,batch_size):
+        return in_image,gt_image
+
+    def random_batch(self,batch_size, option):
         in_image=[]
         gt_image=[]
         cur_idx = random.random(0,max_idx)
         for i in range(batch_size):
             path =self.path+self.files[cur_idx]
-            i_image,g_image=self.get_batch_inputs(path,cur_idx)
+            i_image,g_image=self.get_batch_inputs(path,cur_idx, option)
             in_image.append(i_image)
             gt_image.append(g_image)
             cur_idx = (cur_idx+1)%self.max_idx
@@ -79,7 +90,7 @@ class Dataset:
     max_idx=0
     files=[] 
 
-
+'''
 def main():
     a=Dataset("images/",(512,512),(1024,1024))
     b,c=a.next_batch(2)
@@ -87,3 +98,4 @@ def main():
     print(c.shape)
     
 main()
+'''
