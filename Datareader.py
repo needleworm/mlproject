ef71c52c05c0d321967253d3a7436c9085bb7085
jin8 <kwak.jinmyung@kaist.ac.kr>
2017-05-25 01:15:19 +0900

    add noise function

--- Datareader.py
+++ Datareader.py
@@ -3,23 +3,22 @@ import scipy.misc
 import os
 from PIL import Image
 import numpy as np
-import scipy as sp
-import skimage 
+from Degradation import degrade, im2double
 import random
 
 
+
+
 class Dataset:
-    input_shape = []
-    gt_shape = []
-    path = ""
-    files = []
-    max_idx = 0
-    cur_idx = 0
 
     def __init__(self, path, input_shape=(1024,1024), gt_shape=(1024,1024)):
-        self.input_shape=input_shape
-        self.gt_shape=gt_shape
+
+        self.input_shape = input_shape
+        self.gt_shape = gt_shape
         self.path = path
+        self.cur_idx = 0
+        self.max_idx = 0
+        self.files = []
         if not os.path.exists(path):
             print("path is wrong!")
         else:
@@ -29,6 +28,7 @@ class Dataset:
                     self.max_idx += 1
 
     def read_image(self, path, size, option):   # this function reads image as float64
+
         if option==64:
             image =(scipy.misc.imread(path)).astype(float)
             ret=scipy.misc.imresize(image,size).astype(float)
@@ -44,15 +44,24 @@ class Dataset:
        return ((image*255)/np.max(image)).astype('uint8')
 
     def get_batch_inputs(self, path, idx, option):
-        path = self.path+self.files[idx]
         i_image = self.read_image(path,self.input_shape, option)
+        # noise model
+        if (idx % 3 == 0):
+            i_image = degrade(i_image, ['blur', 'noise', 'saturate', 'compress'])
+        elif (idx % 3 == 1):
+            i_image = degrade(i_image, ['noise', 'saturate', 'compress'])
+        else:
+            i_image = degrade(i_image, ['downscale', 'noise', 'compress'])
+
         if option==64:
-            poissonNoise = np.random.poisson(50, i_image.shape).astype(float) # noise model
+            i_image = i_image.astype(float)
+
         elif option==32:
-            poissonNoise = np.random.poisson(50, i_image.shape).astype('float32') # noise model
-        i_image=i_image+poissonNoise # add noise to image 
-        g_image = self.read_image(path,self.gt_shape, option)
-        return i_image,g_image
+            i_image = i_image.astype('float32')
+
+        g_image = self.read_image(path, self.gt_shape, option)
+        g_image = im2double(g_image)
+        return i_image, g_image
 
     def next_batch(self, batch_size, option):
         in_image=[]
@@ -60,7 +69,7 @@ class Dataset:
         cur_idx=self.cur_idx
         for i in range(batch_size):
             path = self.path+self.files[cur_idx]
-            i_image,g_image = self.get_batch_inputs(path, cur_idx, option)
+            i_image, g_image = self.get_batch_inputs(path, cur_idx, option)
    # for debug
    #        formatted = self.change_format(i_image)
    #        ret = Image.fromarray(formatted)
@@ -79,20 +88,16 @@ class Dataset:
     def random_batch(self,batch_size, option):
         in_image=[]
         gt_image=[]
-        cur_idx = int(random.random() * self.max_idx)
+        cur_idx = random.randint(0, self.max_idx)
         for i in range(batch_size):
-            path =self.path+self.files[cur_idx]
-            i_image,g_image=self.get_batch_inputs(path,cur_idx, option)
+            path = self.path + self.files[cur_idx]
+            i_image, g_image = self.get_batch_inputs(path,cur_idx, option)
             in_image.append(i_image)
             gt_image.append(g_image)
             cur_idx = (cur_idx+1)%self.max_idx
         in_image = np.array(in_image)
-        gt_image=np.array(gt_image)
-        self.cur_idx=cur_idx # update for next batching 
-        return in_image,gt_image        
+        gt_image = np.array(gt_image)
+        self.cur_idx = cur_idx # update for next batching
+        return in_image, gt_image
 
-    input_shape=None
-    gt_shape=None
-    cur_idx=0
-    max_idx=0
-    files=[] 
+
