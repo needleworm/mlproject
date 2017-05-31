ad041ea4a39f4e34b746c8b29db6ab3b636c68cc
leesera <qseralee@gmail.com>
2017-05-31 17:01:02 +0900

    w/ multithreading

--- images/crawl.rb
+++ images/crawl.rb
@@ -3,12 +3,13 @@ categories["artifact11"] = "http://imagenet.stanford.edu/api/text/imagenet.synse
 categories["mics11"] = "http://image-net.org/api/text/imagenet.synset.geturls?wnid=n13912260"
 
 categories.keys.each do |key|
-  #`wget #{url}`
+  Thread.new{
+  `wget #{categories[key]}`
   `mkdir #{key}`
-
   f = File.new categories[key].split("/")[-1],"r"
   while not(f.eof?)
     img = f.readline()
     `cd #{key};wget #{img}`
   end
+  }
 end
