7125b4afe33bb9175c15e754a3fe59a3552c9800
needleworm <bhban@kakao.com>
2017-05-31 16:44:53 +0900

    Add category crawler

--- images/crawl.rb
+++ images/crawl.rb
@@ -1,13 +1,13 @@
-total = 2000
-dup = []
-n = 0
-while n < total
-  jp = "#{n}.jpeg" 
-  cmd = "wget https://source.unsplash.com/random/1024x1024 -O #{jp}"
-  `#{cmd}`
-  md5 = `md5 #{jp}`.split[-1]
-  if not dup.include? md5
-    dup.push md5
-    n += 1
+categories = {}
+categories["artifact"] = "http://imagenet.stanford.edu/api/text/imagenet.synset.geturls?wnid=n00021939"
+
+categories.keys.each do |key|
+  #`wget #{url}`
+  `mkdir #{key}`
+
+  f = File.new categories[key].split("/")[-1],"r"
+  while not(f.eof?)
+    img = f.readline()
+    `cd #{key};wget #{img}`
   end
 end
